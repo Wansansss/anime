a947f5b0efd4065a45d6d810c1b78c8429df369e
Wansansss <ridhwanardiansyah@gmail.com>
2023-12-26 21:21:51 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "cdn.myanimelist.net"
+            },
+            {
+                hostname: "avatars.githubusercontent.com"
+            },
+            {
+                protocol: "https",
+                hostname: "platform-lookaside.fbsbx.com"
+            },
+            {
+                hostname: "lh3.googleusercontent.com"
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
