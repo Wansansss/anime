756574c08f0383d866b3883e152b54783c38e7ab
Wansansss <ridhwanardiansyah@gmail.com>
2023-12-26 23:28:56 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,14 +9,26 @@ const nextConfig = {
                 hostname: "avatars.githubusercontent.com"
             },
             {
-                protocol: "https",
-                hostname: "platform-lookaside.fbsbx.com"
-            },
-            {
                 hostname: "lh3.googleusercontent.com"
             }
         ]
     }
 }
 
-module.exports = nextConfig
+module.exports = {
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/auth/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
+  };
+export default nextConfig
