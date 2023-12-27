b01992d86e8d2c497dc7f49e5b00fba994cf26dd
Wansansss <ridhwanardiansyah@gmail.com>
2023-12-28 03:12:51 +0700

    Update prisma.js

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,10 +1,9 @@
 import { PrismaClient } from "@prisma/client/";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
 let prisma;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient().$extends(withAccelerate());
+  prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
