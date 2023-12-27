deba1d65da92add18150f3bf22c0dedf333448dc
Wansansss <ridhwanardiansyah@gmail.com>
2023-12-28 03:46:00 +0700

    Update route.js

--- src/app/api/v1/comment/route.js
+++ src/app/api/v1/comment/route.js
@@ -1,11 +1,13 @@
+import prisma from "@/libs/prisma";
 
-import prisma from "@/libs/prisma"
+export async function POST(request) {
+  const { anime_mal_id, user_email, comment, username, anime_title } =
+    await request.json();
+  const data = { anime_mal_id, user_email, comment, username, anime_title };
 
-export async function POST(request){
-    const {anime_mal_id, user_email, comment, username, anime_title} = await request.json()
-    const data = {anime_mal_id, user_email, comment, username, anime_title}
-
-    const createComment = await prisma.comment.create ({ data })
-    if (!createComment) return Response.json({ status: 500, isCreated: false})
-    else return Response.json({ status: 200, isCreated: true})
+  const createComment = await prisma.comment
+    .create({ data })
+    .withAccelerateInfo();
+  if (!createComment) return Response.json({ status: 500, isCreated: false });
+  else return Response.json({ status: 200, isCreated: true });
 }
