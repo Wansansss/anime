42dad5ccd01d3d0941ae14b41c0aec55cb9c5d7a
Wansansss <ridhwanardiansyah@gmail.com>
2023-12-28 03:10:37 +0700

    Update route.js

--- src/app/api/v1/collection/route.js
+++ src/app/api/v1/collection/route.js
@@ -6,6 +6,6 @@ export async function POST(request){
     const data = {anime_mal_id, user_email, anime_image, anime_title }
 
     const createCollection = await prisma.collection.create ({ data })
-    if (!createCollection) return Response.json({ status: 500, isCreated: false})
+    if (!createCollection) return Response.json({ status: 400, isCreated: false})
     else return Response.json({ status: 200, isCreated: true})
 }
