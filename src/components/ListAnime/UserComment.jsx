34dc56c2c7f99d302d602e5b01ceec514f71a333
Wansansss <ridhwanardiansyah@gmail.com>
2023-12-28 14:04:58 +0700

    Update UserComment.jsx

--- src/components/ListAnime/UserComment.jsx
+++ src/components/ListAnime/UserComment.jsx
@@ -1,24 +1,17 @@
 import prisma from "@/libs/prisma";
 
 const UserComment = async ({ anime_mal_id }) => {
-  const comments = await prisma.comment
-    .findMany({
-      where: { anime_mal_id },
-    })
-    .withAccelerateInfo();
+  const comments = await prisma.comment.findMany({ where: { anime_mal_id } });
 
   return (
     <div className="grid grid-cols-2 gap-4 mb-4">
       {comments.map((comment) => {
-        return (
-          <div
-            key={comment.id}
-            className=" font-bold w-full bg-emerald-100 rounded-full text-lg p-4 "
-          >
-            <p>{comment.username} : </p>
-            <p>{comment.comment}</p>
-          </div>
-        );
+        return(
+        <div key={comment.id} className=" font-bold w-full bg-emerald-100 rounded-full text-lg p-4 ">
+          <p>{comment.username} : </p>
+          <p>{comment.comment}</p>
+        </div>
+        )
       })}
     </div>
   );
